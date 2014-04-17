9e6f9013aa38870ab7186283b728a296a7a86f33
mrgriever93 <alexandre.santos1@hotmail.com>
2014-04-17 12:12:51 +0100

    mediagest

--- application/views/mediagest/header.php
+++ application/views/mediagest/header.php
@@ -2,7 +2,7 @@
 <html lang="">
 <head>
 	<meta charset="utf-8">
-	<title>Sosoares - MediaGest</title>
+	<title>Gres Panaria - MediaGest</title>
 	<meta name="description" content="" />
 	<meta name="keywords" content="" />
 	<meta name="robots" content="" />
@@ -22,87 +22,17 @@
 <?php foreach($js_files as $file): ?>
 	<script src="<?php echo $file; ?>"></script>
 <?php endforeach; ?>
-<!--<link type="text/css" rel="stylesheet" href="<?php echo base_url() ?>assets/indelague/css/change_order.css" />
-<script type="text/javascript" src="<?php echo base_url() ?>assets/indelague/js/change_order.js" ></script>-->
 <script type="text/javascript" src="<?php echo base_url() ?>assets/duplicate_item/js/duplicate_item.js" ></script>
 <script type="text/javascript" src="<?php echo base_url() ?>assets/mediagest/js/form_tabs.js" ></script>
 </head>
 <body>
 <div class="testing">
-<!--<header class="main">
-	<h1><strong>Media</strong> Gest</h1>
-	<input type="text" value="search" />
-</header>-->
 <section class="user">
 	<div class="profile-img">
-		<p><img src="<?php echo base_url() ?>assets/mediagest/images/logo.png" alt="" width="174" /> </p>
+		<p><img src="<?php echo base_url() ?>assets/mediagest/images/logo.png" alt="" width="279" /> </p>
 	</div>
 	<div class="buttons">
 		<button class="ico-font">&#59157;</button>
-			<!--<span class="button dropdown">
-			<a href="#">Notifications <span class="pip">4</span></a>
-			<ul class="notice">
-				<li>
-					<hgroup>
-						<h1>You have a new task</h1>
-						<h2>Report web statistics week by week.</h2> 
-					</hgroup>
-					<p><span>14:24</span></p>
-				</li>
-				<li>
-					<hgroup>
-						<h1>New comment</h1>
-						<h2>Comment on <em>About page</em> by Darren.</h2> 
-					</hgroup>
-					<p><span>11:04</span></p>
-				</li>
-				<li>
-					<hgroup>
-						<h1>Broken link</h1>
-						<h2>We've spotted a broken link on the <em>Blog page</em>.</h2> 
-					</hgroup>
-					<p><span>10:46</span></p>
-				</li>
-				<li>
-					<hgroup>
-						<h1>User report</h1>
-						<h2><em>Lee Grant</em> has been promoted to admin.</h2> 
-					</hgroup>
-					<p><span>09:57</span></p>
-				</li>
-			</ul>
-		</span> 
-		<span class="button dropdown">
-			<a href="#">Inbox <span class="pip">6</span></a>
-			<ul class="notice">
-				<li>
-					<hgroup>
-						<h1>Hi, I need a favour</h1>
-						<h2>John Doe</h2>
-						<h3>Lorem ipsum dolor sit amet, consectetuer sed aidping putamus delo de sit felume...</h3>
-					</hgroup>
-					<p><span>11:24</span></p>
-				</li>
-				<li>
-					<hgroup>
-						<h1><span class="icon">&#59154;</span>Hi, I need a favour</h1>
-						<h2>John Doe</h2>
-						<h3>Lorem ipsum dolor sit amet, consectetuer sed aidping putamus delo de sit felume...</h3>
-					</hgroup>
-					<p><span>11:24</span></p>
-				</li>
-				<li>
-					<hgroup>
-						<h1><span class="icon">&#59154;</span>Hi, I need a favour</h1>
-						<h2>John Doe</h2>
-						<h3>Lorem ipsum dolor sit amet, consectetuer sed aidping putamus delo de sit felume...</h3>
-					</hgroup>
-					<p><span>11:24</span></p>
-				</li>
-			</ul>
-		</span> 
-		<span class="button">Live</span>
-		 <span class="button">Help</span>-->
 		 <span class="button blue"><a href='<?php echo site_url("login/logout")?>'>Logout</a></span>
 	</div>
 </section>    
