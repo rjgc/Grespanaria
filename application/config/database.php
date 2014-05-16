2080cd701ee29bb045db264ce6a4bc7fe259d029
mrgriever93 <alexandre.santos1@hotmail.com>
2014-05-16 15:28:11 +0100

    database

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '123';
-$db['default']['database'] = 'grespanaria';
+$db['default']['hostname'] = 'critecns.com';
+$db['default']['username'] = 'crtns25g_gestor';
+$db['default']['password'] = 'rGGST}T6vm=@';
+$db['default']['database'] = 'crtns25g_grespanaria';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
